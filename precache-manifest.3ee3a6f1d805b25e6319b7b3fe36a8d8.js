ae7f85b569e63e34e7833b3cd2718d0dc6c13856
mangesh <mshanit@gmail.com>
2020-09-21 16:24:34 +0530

    Updates

---
+++ precache-manifest.3ee3a6f1d805b25e6319b7b3fe36a8d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99cac4c4fc062f6671fc69687f388b3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "481742cee30a3a1fb28b",
+    "url": "/static/css/main.2598c454.chunk.css"
+  },
+  {
+    "revision": "01c51514715ee82859c1",
+    "url": "/static/js/2.504a7afd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.504a7afd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "481742cee30a3a1fb28b",
+    "url": "/static/js/main.c82bd08e.chunk.js"
+  },
+  {
+    "revision": "1c37aaa004d8ca449616",
+    "url": "/static/js/runtime-main.bb89a948.js"
+  }
+]);
