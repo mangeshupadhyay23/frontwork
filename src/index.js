d5af7ca8c1b09489ef5755071003a563761ddce6
mangesh <mshanit@gmail.com>
2020-09-22 09:25:23 +0530

    git deploy

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {BrowserRouter} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
 const app=(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={'http://frontwork.surge.sh'}>
         <App/>
     </BrowserRouter>
 );
