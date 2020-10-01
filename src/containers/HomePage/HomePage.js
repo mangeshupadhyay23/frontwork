4a030d6eba2e16fc4a026f806ecba7218204329b
Hardik Singh <hardiksingh297@gmail.com>
2020-10-02 00:42:06 +0530

    Changed card frequency

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,27 +1,20 @@
-import React from 'react';
-import Card from './Cards';
-import {Paper,Grid} from '@material-ui/core';
+import React from "react";
+import Card from "./Cards";
+import { Paper, Grid } from "@material-ui/core";
 
-
-
-class HomePage extends React.Component{
-    render(){
-        return(
-            <div >
-                <Paper style={{margin:"80px",padding:"24px"}}>
-                    <Grid container style={{justifyContent:"space-around"}}>
-                        <Card/> 
-                        <Card/>
-                        <Card/>
-                        <Card/>
-                        <Card/>
-                        <Card/>
-                        <Card/>
-                    </Grid>
-                </Paper>
-            </div>
-        );
-    }
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div>
+        <Paper style={{ margin: "80px", padding: "24px" }}>
+          <Grid container style={{ justifyContent: "space-around" }}>
+            <Card />
+            <Card />
+          </Grid>
+        </Paper>
+      </div>
+    );
+  }
 }
 
 export default HomePage;
